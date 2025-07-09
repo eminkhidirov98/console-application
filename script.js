4d8123d74951ec08c8e4f93aec13e1ff5a07de5c
Emin <eminkhidirov98@gmail.com>
2025-07-09 17:01:27 +0400

    Add personal movie DB with movie names and ratings

---
+++ script.js
@@ -0,0 +1,46 @@
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'd
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const movieName = prompt('Один из последних просмотренных фильмов?', '');
+const movieRating = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[movieName] = movieRating;
+
+const movieName2 = prompt('Один из последних просмотренных фильмов?', '');
+const movieRating2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[movieName2] = movieRating2;
+
+console.log(personalMovieDB);
