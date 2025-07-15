e19dd74c3b0ed9ed1e38a109009202f5b46e7256
Emin <eminkhidirov98@gmail.com>
2025-07-15 08:10:27 +0400

    feat: implement showMyDB and writeYourGenres functions

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 /* Задание на урок:
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+1) Первую часть задания повторить по уроку
 
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
 
-4) Потренироваться и переписать цикл еще двумя способами*/
+P.S. Функции вызывать не обязательно*/
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -24,34 +33,60 @@ const personalMovieDB = {
   privat: false,
 };
 
+function showMyDB() {
+  if (personalMovieDB.privat === false) {
+    console.log(personalMovieDB);
+  }
+}
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    const genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+    if (genre === null || genre.trim() === '') {
+      console.log('Некоректный ввод. Попробуйте снова.');
+      i--;
+      continue;
+    }
+    personalMovieDB.genres.push(genre);
+  }
+}
+writeYourGenres();
+
 const numbersOfQuestions = 2;
 
-for (let i = 0; i < numbersOfQuestions; i++) {
-  const movieName = prompt('Один из последних просмотренных фильмов', '');
-  const movieRaiting = prompt('На сколько оуените его?', '');
+function rememberMyFilms() {
+  for (let i = 0; i < numbersOfQuestions; i++) {
+    const movieName = prompt('Один из последних просмотренных фильмов', '');
+    const movieRating = prompt('На сколько оуените его?', '');
 
-  if (
-    movieName === null ||
-    movieRaiting === null ||
-    movieName.trim() === '' ||
-    movieRaiting.trim() === '' ||
-    movieName.length > 50
-  ) {
-    console.log('Ошибка! Введите данные коректно.');
-    i--;
-    continue;
+    if (
+      movieName === null ||
+      movieRating === null ||
+      movieName.trim() === '' ||
+      movieRating.trim() === '' ||
+      movieName.length > 50
+    ) {
+      console.log('Ошибка! Введите данные коректно.');
+      i--;
+      continue;
+    }
+
+    personalMovieDB.movies[movieName] = movieRating;
   }
+}
+rememberMyFilms();
 
-  personalMovieDB.movies[movieName] = movieRaiting;
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+  } else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+  } else {
+    console.log('Произошла ошибка');
+  }
 }
-if (personalMovieDB.count < 10) {
-  console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-  console.log('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-  console.log('Вы киноман');
-} else {
-  console.log('Произошла ошибка');
-}
+detectPersonalLevel();
 
 console.log(personalMovieDB);
